0a086e296e6ba3f5457b8fa31a6979d4874bf96a
ElGonan <alanpagzz@outlook.com>
2025-04-23 00:51:23 -0600

    SQL part done

---
+++ misc/querys.sql
@@ -0,0 +1,21 @@
+create TABLE Alumno (
+  alumno_ID SERIAL PRIMARY KEY,
+  alumno_name VARCHAR(100) NOT NULL,
+  alumno_phone BIGINT NOT NULL CHECK (alumno_phone BETWEEN 1000000000 AND 9999999999)
+);
+
+create TABLE Clase (
+  clase_ID SERIAL PRIMARY KEY,
+  instructor varchar(50) NOT NULL,
+  fecha_hora date NOT NULL,
+  capacidad_Clase int NOT NULL CHECK (capacidad_Clase > 0),
+  nombre_clase varchar(100) NOT NULL
+);
+
+
+create TABLE Alumno_Clase (
+  inscripcion_ID SERIAL PRIMARY KEY,
+  alumno_ID INT NOT NULL REFERENCES Alumno(alumno_ID),
+  clase_ID INT NOT NULL REFERENCES Clase(clase_ID),
+  UNIQUE (alumno_id, clase_id)
+);
