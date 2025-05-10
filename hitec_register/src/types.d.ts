515e9d20d9a45e5ee32bbce0c1727cf83915b49b
XxGonanxX <alanpagzz@outlook.com>
2025-05-10 02:00:55 -0600

    types added

---
+++ hitec_register/src/types.d.ts
@@ -0,0 +1,13 @@
+type User = {
+    id : number
+    name : string
+    phone : number
+}
+
+type Class = {
+    id : number
+    instructor : string
+    date : string
+    capacity : number
+    className : string
+}
