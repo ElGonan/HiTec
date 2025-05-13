c8e653e39ab8d60a2d3aea2e9c40413a4e26519c
XxGonanxX <alanpagzz@outlook.com>
2025-05-12 22:26:10 -0600

    register user now complete

--- misc/querys.sql
+++ misc/querys.sql
@@ -1,15 +1,19 @@
-create TABLE Alumno (
-  alumno_ID SERIAL PRIMARY KEY,
-  alumno_name VARCHAR(100) NOT NULL,
-  alumno_phone UNIQUE BIGINT NOT NULL CHECK (alumno_phone BETWEEN 1000000000 AND 9999999999)
+CREATE TABLE Alumno (
+  alumno_ID BIGINT PRIMARY KEY,
+  alumno_name TEXT NOT NULL,
+  alumno_matricula TEXT NOT NULL UNIQUE,
+  alumno_phone BIGINT UNIQUE NOT NULL CHECK (alumno_phone BETWEEN 1000000000 AND 9999999999)
 );
+
 
 create TABLE Clase (
   clase_ID SERIAL PRIMARY KEY,
-  instructor varchar(50) NOT NULL,
+  instructor TEXT NOT NULL,
   fecha_hora date NOT NULL,
+  area TEXT NOT NULL,
+  lugar TEXT NOT NULL,
   capacidad_Clase int NOT NULL CHECK (capacidad_Clase >= 0),
-  nombre_clase varchar(100) NOT NULL
+  nombre_clase TEXT NOT NULL
 );
 
 
@@ -19,3 +23,9 @@ create TABLE Alumno_Clase (
   clase_ID INT NOT NULL REFERENCES Clase(clase_ID),
   UNIQUE (alumno_id, clase_id)
 );
+
+
+/*
+To avoid vulnerabilities, all varchars where replaced with TEXT
+
+*/
