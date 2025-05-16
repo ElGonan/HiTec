f27e631053670e14b904bed7598ecddb3d24a95f
ElGonan <alanpagzz@outlook.com>
2025-05-16 16:58:17 -0600

    caos

--- src/pages/Area.tsx
+++ src/pages/Area.tsx
@@ -34,7 +34,7 @@ const Area = () => {
             <div>
                 {areas.map((areas, index) => (
                     <div key={index} className="bg-white shadow-md rounded-lg p-4 mb-4">
-                        <h2 className="text-xl font-semibold">{areas.area}</h2>
+                        <button>{areas.area}</button>
                     </div>
                 ))}
             </div>
