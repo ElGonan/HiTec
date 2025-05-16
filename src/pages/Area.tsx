4b49e26b970eceb6d93e1bfe1a624b3728cc8724
ElGonan <alanpagzz@outlook.com>
2025-05-16 16:49:19 -0600

    now areas are deployed

--- src/pages/Area.tsx
+++ src/pages/Area.tsx
@@ -1,16 +1,44 @@
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import supabaseGetTime from '../lib/supabaseGetTime';
 
 const Area = () => {
-    const navigate = useNavigate();
     const location = useLocation();
+    const [areas, setAreas] = useState<[]>([]);
     const { alumno_id, time } = location.state as { alumno_id: number; time: number };
+
+    const getClases = async () => {
+    
+        const {data, error} = await supabaseGetTime(time);
+        if (error) {
+            alert(error.message);
+            return;
+        }
+        if (data) {
+            console.log(data);
+            setAreas(data);
+        }
+
+        
+
+
+
+    }
+    useEffect(() => {
+        getClases();
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Area</h1>
-            <p className="text-lg">This is the Area page.</p>
-            <p className="text-lg">Alumno ID: {alumno_id}</p>
-            <p className="text-lg">Time: {time}</p>
+            <h1 className="text-2xl font-bold mb-4">Seleccione una Área</h1>
+            <div>
+                {areas.map((areas, index) => (
+                    <div key={index} className="bg-white shadow-md rounded-lg p-4 mb-4">
+                        <h2 className="text-xl font-semibold">{areas.area}</h2>
+                    </div>
+                ))}
+            </div>
+
         </div>
     )
 
