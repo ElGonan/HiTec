151997eb707b9f416554134beac6520df6bf6e4b
ElGonan <alanpagzz@outlook.com>
2025-08-04 21:02:05 -0600

    funcion corregida

--- misc/querys.sql
+++ misc/querys.sql
@@ -37,3 +37,49 @@ create TABLE Alumno_Clase (
 To avoid vulnerabilities, all varchars where replaced with TEXT
 
 */
+
+
+/*
+Other queries
+*/
+
+CREATE OR REPLACE FUNCTION decrementar_capacidad(p_clase_id int)
+RETURNS json AS $$
+DECLARE
+  capacidad_actual int;
+BEGIN
+  SELECT capacidad_clase INTO capacidad_actual
+  FROM clase WHERE clase_id = p_clase_id FOR UPDATE;
+  
+  IF capacidad_actual > 0 THEN
+    UPDATE clase SET capacidad_clase = capacidad_clase - 1
+    WHERE clase_id = p_clase_id;
+    RETURN json_build_object('success', true);
+  ELSE
+    RETURN json_build_object('error', 'No hay cupo');
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+CREATE OR REPLACE FUNCTION increment() 
+RETURNS integer AS $$
+BEGIN
+  RETURN 1; -- Retorna +1 para sumar 1 a capacidad_clase
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+CREATE OR REPLACE FUNCTION decrement() 
+RETURNS integer AS $$
+BEGIN
+  RETURN -1; -- Retorna -1 para restar 1 a capacidad_clase
+END;
+$$ LANGUAGE plpgsql;
+
+
