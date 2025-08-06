686c5d0a5f7e7c05fc1589582d0c87a7859a1af8
ElGonan <alanpagzz@outlook.com>
2025-08-06 16:34:09 -0600

    vista de horarios mejorada

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById('root')!).render(
               fontSize: '8px',
               padding: '5px',
             }}>
-              <p>v0.9.6 Beta</p>
+              <p>v0.9.7 Beta</p>
             </div>
     </footer>
   </StrictMode>,
